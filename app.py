3a80d64cf7d9a9157cef57715d1bc86209343e26
GinesNC <gines_nc@hotmail.com>
2018-11-02 21:46:52 +0100

    para heroku

--- app.py
+++ app.py
@@ -5,14 +5,20 @@ class HelloWorld(object):
     def index(self):
         return "Hello World!"
 
-#cherrypy.quickstart(HelloWorld())
+config = {
+    'global': {
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': int(os.environ.get('PORT', 5000)),
+    }
+    }
+cherrypy.quickstart(HelloWorld(),'/',config=config)
 #cherrypy.config.update({'server.socket_host': '64.72.221.48',
-                        #'server.socket_port': 80,
-                      # })
+#                        'server.socket_port': 80,
+#                      })
 
-wsgi_app = cherrypy.Application(HelloWorld(), '/')
-if __name__ == '__main__':
-	from wsgiref.simple_server import make_server
-
-	httpd = make_server('', 6600, wsgi_app)
-    httpd.serve_forever()
+# wsgi_app = cherrypy.Application(HelloWorld(), '/')
+# if __name__ == '__main__':
+# 	from wsgiref.simple_server import make_server
+#
+# 	httpd = make_server('', 6600, wsgi_app)
+#     httpd.serve_forever()
