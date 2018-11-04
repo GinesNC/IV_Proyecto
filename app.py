2bcaacef4689446e467cf01c5d748f0cd7ae2659
GinesNC <gines_nc@hotmail.com>
2018-11-04 19:53:32 +0100

    funciona despliegue

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class WebLSP(object):
     def index(self,valor="No se pasa valor"):
         return {"status":'OK', "valor" : valor}
 
-    
+
 
 config = {
     'global': {
@@ -15,13 +15,3 @@ config = {
     }
     }
 cherrypy.quickstart(HelloWorld(),'/',config=config)
-#cherrypy.config.update({'server.socket_host': '64.72.221.48',
-#                        'server.socket_port': 80,
-#                      })
-
-# wsgi_app = cherrypy.Application(HelloWorld(), '/')
-# if __name__ == '__main__':
-# 	from wsgiref.simple_server import make_server
-#
-# 	httpd = make_server('', 6600, wsgi_app)
-#     httpd.serve_forever()
