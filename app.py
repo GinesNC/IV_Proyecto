60b9f9c21b6172e7c8e6a6b19f58a5917467222e
GinesNC <gines_nc@hotmail.com>
2018-12-09 19:03:13 +0100

    faltaba una ,

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class WebLSP(object):
         valordat= self.datos("Un titulo", "2018", "0", "libro")
         return {"status":'OK', "rutas":{
             "insertardatos":{"nombre": "/insertardatos","json_ruta": valorejem},
-            "datos":{"nombre":"/datos?titulo=Un+titulo&year=2018&mi_puntuacion=0&tipo=libro", "json_ruta": valordat}
+            "datos":{"nombre":"/datos?titulo=Un+titulo&year=2018&mi_puntuacion=0&tipo=libro", "json_ruta": valordat},
             "status": {"status":"OK"}
             }
             }
