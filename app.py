a4155d50cdbf349e08076bc60508f72966a877d8
GinesNC <gines_nc@hotmail.com>
2019-01-02 11:15:48 +0100

    Cambiado el puerto del 8443-->80

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class WebLSP(object):
 config = {
     'global': {
         'server.socket_host': '0.0.0.0',
-        'server.socket_port': int(os.environ.get('PORT', 8443)),
+        'server.socket_port': int(os.environ.get('PORT', 80)),
     }
     }
 cherrypy.quickstart(WebLSP(),'/',config=config)
